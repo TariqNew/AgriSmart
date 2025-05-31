c813aebec45c6c48f1ffa3adf63bf64cb0003539
TariqNew <marandumashaka78@gmail.com>
2025-05-31 15:36:36 +0300

    Adding the openweather to the server

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ const express = require('express');
 // const sendSms = require('./smsSender');
 const ussdHandler = require('./ussd-sms-backend/ussdHandler');
 const smsHandler = require('./agribot/smsHandler');
+const weather = require('./openweather/openweather');
+const cors = require('cors')
 const dotenv = require("dotenv").config()
+
+
 
 //initialize port variable 
 const PORT = process.env.PORT;
 
 const app = express();
+
+
+// Enable CORS for all routes
+app.use(cors());
 
 //body parser
 app.use(express.json());
@@ -19,6 +27,10 @@ app.post('/incoming-sms', smsHandler);
 
 //Endpoint for ussd 
 app.post('/ussd', ussdHandler)
+
+//Endpoint for weather
+app.get('/weather', weather);
+
 
 
 app.listen(PORT, (req, res) => {
